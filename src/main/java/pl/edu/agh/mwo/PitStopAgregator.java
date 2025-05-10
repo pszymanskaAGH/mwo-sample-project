235de324eac69868cac6427f4030d12cd76cb59a
PiotrPoloczek <piotr@PiotrPoloczeks-MacBook-Pro.local>
2025-05-10 15:14:04 +0200

    add the PitStopAgregator

---
+++ src/main/java/pl/edu/agh/mwo/PitStopAgregator.java
@@ -0,0 +1,16 @@
+package pl.edu.agh.mwo;
+
+import java.util.List;
+
+public class PitStopAgregator {
+
+    private List<PitstopData> pitstopDataList;
+
+    public PitStopAgregator(String json){
+        // TODO: Add mapper from string to list of the pitstopData
+    }
+
+    public List<PitstopData> getPitstopDataList() {
+        return pitstopDataList;
+    }
+}
