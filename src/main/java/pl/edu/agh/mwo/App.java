286ea8bab5b80dd1125b2b48806c64594e7f940c
PiotrPoloczek <piotr@PiotrPoloczeks-MacBook-Pro.local>
2025-05-10 15:42:56 +0200

    add reading from arguments in command

--- src/main/java/pl/edu/agh/mwo/App.java
+++ src/main/java/pl/edu/agh/mwo/App.java
@@ -3,8 +3,13 @@ package pl.edu.agh.mwo;
 public class App {
     public static void main(String[] args) {
 
-        // path to file
-        String pathToFile = "json/pit.json";
+        // Sprawdzenie czy argument został przekazany
+        if (args.length < 1) {
+            System.out.println("Użycie: java -jar program.jar <ścieżka_do_pliku_json>");
+            return;
+        }
+
+        String pathToFile = args[0]; // pierwszy argument z linii komend
 
         // read the json from file to String
         JsonReaderFromFile reader = new JsonReaderFromFile(pathToFile);
@@ -13,6 +18,7 @@ public class App {
         // create PitStopAgregator
         PitStopAgregator pitStopAgregator = new PitStopAgregator(reader.getJson());
 
+        // print results
         System.out.println(pitStopAgregator.countPitStops());
         System.out.println(pitStopAgregator.getTheLongestPitStopWithDriverNumber());
     }
