0022d8f36898a64f0441cc5bb66380b26a69538c
mdebski <midebski@student.agh.edu.pl>
2025-05-10 15:24:22 +0200

    add objectmapper to convert string from json to pitstopdata class

---
+++ src/main/java/pl/edu/agh/mwo/MapperToPitstopData.java
@@ -0,0 +1,19 @@
+package pl.edu.agh.mwo;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.util.List;
+
+public class MapperToPitstopData {
+
+    public List<PitstopData> mapperToPitstopData(String jsonInString)  {
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            return mapper.readValue(jsonInString, new TypeReference<>() {});
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
